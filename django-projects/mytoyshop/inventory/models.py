698ddb6f8b3fcce32aa4ed5779732859432fb7f8
Sambhavi PD <sambhavi@golittlebig.com>
2020-10-06 16:48:46 +0530

    Added models to mytoyshop

--- django-projects/mytoyshop/inventory/models.py
+++ django-projects/mytoyshop/inventory/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+ 
 # Create your models here.
+ 
+# Table to hold Category details
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name= \
+       'Category name')
+    description = models.TextField(verbose_name = \
+       'Detailed description')
+ 
+    class Meta:
+       unique_together = ['name',]
+
+# Table to hold details about each Toy
+class Toy(models.Model):
+    name = models.CharField(max_length=100, verbose_name= \
+       'Toy name')
+    category = models.ForeignKey(Category, \
+       on_delete=models.CASCADE, \
+       verbose_name='Category of Toy')
+    description = models.TextField(verbose_name = \
+       'Detailed description')
+    cost = models.DecimalField(max_digits=6, decimal_places=2, \
+       verbose_name='Cost of Toy in INR')
+    brand = models.CharField(max_length=100, verbose_name= \
+       'Brand of Toy')
+    ACTIVITY_TYPE_CHOICES = (
+       ('Indoor', 'Indoor'),
+       ('Outdoor', 'Outdoor'),
+    )
+    activity_type = models.CharField(max_length=20, choices=\
+       ACTIVITY_TYPE_CHOICES, verbose_name='Activity type')
+     
+    class Meta:
+        unique_together = ['name', 'category']
+     
+    def __string__(self):
+        return '{0} {1}'.format(self.name, self.category)
